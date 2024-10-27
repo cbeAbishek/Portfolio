4f779db6fe2f313b3e319028f37ef92980bf94f2
cbeAbishek <agsabhishek1907@gmail.com>
2024-10-27 17:43:02 +0530

    small correction

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ export default function Header() {
     const [toggleMenu, setToggleMenu]  = useState(false);
 
     return <header className="flex justify-between px-5 py-2 bg-primary ">
-        <a className="font-bold text-black" href="#">Abishek</a>
+        <a className="font-bold text-black" h
+        ref="#">Abishek Portfolio</a>
         <nav className="hidden md:block">
             <ul className="flex text-white ">
                 <li><a href="/">Home</a></li>
